3dc374b78ce6d992c7d85d729d78323023926106
andreicvasniuc <andrei.a.cvasniuc@gmail.com>
2020-09-07 18:36:00 +0300

    implemented closing of gallery when browser back button is clicked

--- client/src/app/components/item-details/index.jsx
+++ client/src/app/components/item-details/index.jsx
@@ -3,7 +3,20 @@ import './style.styl';
 import template from './template.html';
 
 class ItemDetailsController {
-  constructor() {
+  constructor($scope) {
+    this.$scope = $scope;
+  }
+
+  clickOnItem() {
+    this.listener = this.$scope.$on('$locationChangeStart', (event, next, current) => {
+         event.preventDefault();
+         this.closeGallery();
+         this.listener();
+    });
+  }
+
+  closeGallery() {
+    window.lgData['lg' + (window.lgData.uid -1)].destroy();
   }
 }
 
