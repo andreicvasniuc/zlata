e0941112cbc60d6c8395e1bfa895678f8d06b4ff
andreicvasniuc <andrei.a.cvasniuc@gmail.com>
2020-09-07 16:07:56 +0300

    * display collection and product names always on mobile except on product detail page * implemented minimization of css and js

--- client/config/webpack.prod.js
+++ client/config/webpack.prod.js
@@ -1,10 +1,12 @@
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
+const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const helpers = require('./helpers');
 
-const commonConfig = require('./webpack.common.admin.js');
-// const commonConfig = require('./webpack.common.app.js');
+// const commonConfig = require('./webpack.common.admin.js');
+const commonConfig = require('./webpack.common.app.js');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
@@ -17,12 +19,14 @@ module.exports = merge(commonConfig, {
     chunkFilename: '[id].[hash].chunk.js'
   },
 
-  // htmlLoader: {
-  //   minimize: false // workaround for ng2
-  // },
-
   optimization: {
-    minimize: false
+    // minimize: false,
+    minimizer: [new TerserJSPlugin({
+      terserOptions: {
+        // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        mangle: false
+      }
+    }), new OptimizeCSSAssetsPlugin({})]
   },
 
   plugins: [
